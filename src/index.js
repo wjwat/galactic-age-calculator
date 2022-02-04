47dc9123e3a87b75492d8dff16bf9140f91ae3df
Will W <wjwat@onionslice.org>
2022-02-04 15:45:18 -0600

    add barebones interactivity to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import $ from 'jquery';
 import './css/yacck.css';
 import './css/styles.css';
+import GalacticAge from './js/galactic-age';
 
-console.log($('title').val());
+const outputSel = $('#output');
+const inputSel = $('#user-age');
+
+function displayAges(inputVal) {
+  let ga = new GalacticAge(inputVal);
+
+  ga.getPlanets().forEach(planet => {
+    outputSel.append(planet + ': ' + ga.getAge(planet) + '<br>');
+  });
+}
+
+$('#find-age').on('click', () => {
+  displayAges(inputSel.val());
+});
