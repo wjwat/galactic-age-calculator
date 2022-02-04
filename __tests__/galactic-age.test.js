7e4d84887b1968d1203d966dd2d1f1eae4526231
Will W <wjwat@onionslice.org>
2022-02-04 12:38:17 -0600

    Test property describing solar years in relation to earth

--- __tests__/galactic-age.test.js
+++ __tests__/galactic-age.test.js
@@ -1,8 +1,24 @@
 import GalacticAge from './../src/js/galactic-age.js';
 
 describe('GalacticAge', () => {
-  test('constructor creates a earthAge property with a value that is passed in', () => {
-    const ga = new GalacticAge(35);
+  const testBodies = [
+    ['mercury', 0.24,],
+    ['venus', 0.62,],
+    ['earth', 1,],
+    ['mars', 1.88,],
+    ['jupiter', 11.86,],
+    ['saturn', 29.48,],
+    ['uranus', 84.07,],
+    ['neptune', 164.90]
+  ];
+  const ga = new GalacticAge(35);
+
+  test('creates a earthAge property with a value that is passed in', () => {
     expect(ga.earthAge).toEqual(35);
   });
+  test.each(testBodies)('create property where bodies[%s] === %d',
+    (planet, solarYear) => {
+      expect(ga.bodies[planet]).toEqual(solarYear);
+    }
+  );
 });
