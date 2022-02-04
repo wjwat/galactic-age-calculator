5f738bf58197dfd5749c98c05a94fc1916e4e2fd
Will W <wjwat@onionslice.org>
2022-02-04 12:56:00 -0600

    PASS test for bodies property of GalacticAge

--- src/js/galactic-age.js
+++ src/js/galactic-age.js
@@ -1,5 +1,15 @@
 export default class GalacticAge {
   constructor(earthAge) {
     this.earthAge = earthAge;
+    this.bodies = {
+      'mercury': 0.24,
+      'venus': 0.62,
+      'earth': 1,
+      'mars': 1.88,
+      'jupiter': 11.86,
+      'saturn': 29.48,
+      'uranus': 84.07,
+      'neptune': 164.90
+    };
   }
 }
