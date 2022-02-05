c52b26b5c354542de70633d0cccaa8002ba8ddcb
Will W <wjwat@onionslice.org>
2022-02-04 18:20:37 -0600

    Add functions for styling of output to user

--- src/index.js
+++ src/index.js
@@ -6,11 +6,38 @@ import GalacticAge from './js/galactic-age';
 const outputSel = $('#output');
 const inputSel = $('#user-age');
 
+function bold(text) {
+  return `<span class='big-and-bold'>${text}</span>`;
+}
+
+function createListItem(item) {
+  return `<li>${item}</li>`;
+}
+
+function clearOutput() {
+  outputSel.text('');
+}
+
+function determineYearsLeftOutput(years, currAge, maxAge) {
+  let outStr = '';
+
+  if (currAge > maxAge) {
+    outStr = `you have lived ${bold(Math.abs(years))} years past your lie expectancy!`;
+  } else {
+    outStr = `you have ${bold(years)} years left to live!`;
+  }
+  return outStr;
+}
+
 function displayAges(inputVal) {
-  let ga = new GalacticAge(inputVal);
+  const ga = new GalacticAge(inputVal);
+  clearOutput();
 
   ga.getPlanets().forEach(planet => {
-    outputSel.append(planet + ': ' + ga.getAge(planet) + '<br>');
+    const yearsLeft = determineYearsLeftOutput(ga.getYearsLeft(planet), inputVal, ga.maxAge);
+    const item = `Your age on ${bold(planet)} is ${bold(ga.getAge(planet))} ` +
+      `years and ${yearsLeft}`;
+    outputSel.append(createListItem(item));
   });
 }
 
